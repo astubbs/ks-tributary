982c65a22c4ae44b1417c8e41161babce7a5aa7f
Antony Stubbs <antony.stubbs@gmail.com>
2020-02-19 15:54:42 +1100

    S! Topology compiles, three suppression techniques

--- streams-module/src/main/java/io/confluent/ps/streams/referenceapp/schools/topology/SchoolTopology.java
+++ streams-module/src/main/java/io/confluent/ps/streams/referenceapp/schools/topology/SchoolTopology.java
@@ -1,82 +1,146 @@
 package io.confluent.ps.streams.referenceapp.schools.topology;
 
-import io.confluent.ps.streams.referenceapp.schools.model.OrgUnit;
-import io.confluent.ps.streams.referenceapp.schools.model.SchStatusCode;
-import io.confluent.ps.streams.referenceapp.schools.model.SchSubtype;
-import io.confluent.ps.streams.referenceapp.schools.model.SchoolId;
-import lombok.Data;
+import io.confluent.ps.streams.referenceapp.schools.model.*;
+import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
+import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.processor.PunctuationType;
-import org.apache.kafka.streams.state.KeyValueIterator;
-import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.*;
 
 import javax.inject.Inject;
 import java.time.Duration;
 
+import static org.apache.kafka.streams.kstream.Suppressed.BufferConfig.unbounded;
+
 public class SchoolTopology {
 
-  public static final String ORG_TOPIC = "org_unit";
+  public static final String ORG_UNIT_TOPIC = "org_unit";
   public static final String SUP_TYPE_TOPIC = "sub_type";
   public static final String STATUS_CODE_TOPIC = "status_code";
   public static final String AGGREGATE_UPDATES_TOPIC = "aggregate_updates";
+  public static final String SCHOOL_AGGREGATE_STORE = "school-aggregate-store";
+
+  private final Duration suppressionWindowTime = Duration.ofSeconds(20);
 
   @Inject
-  public SchoolTopology(StreamsBuilder builder) {
-    KStream<SchoolId, Object> orgTopicStream = builder.stream(ORG_TOPIC).selectKey((k, v) -> ((OrgUnit) v).getSchoolCode());
-    KStream<SchoolId, Object> subTypeStream = builder.stream(SUP_TYPE_TOPIC).selectKey((k, v) -> ((SchSubtype) v).getSchoolCode());
-    KStream<SchoolId, Object> statusCodeStream = builder.stream(STATUS_CODE_TOPIC).selectKey((k, v) -> ((SchStatusCode) v).getSchoolCode());
+  public SchoolTopology(StreamsBuilder builder, StoreProvider storeProvider) {
+    KStream<SchoolId, Object> orgTopicStream = builder.stream(ORG_UNIT_TOPIC)
+            .selectKey((k, v) -> ((OrgUnit) v).getSchoolCode());
+    KStream<SchoolId, Object> subTypeStream = builder.stream(SUP_TYPE_TOPIC)
+            .selectKey((k, v) -> ((SchSubtype) v).getSchoolCode());
+    KStream<SchoolId, Object> statusCodeStream = builder.stream(STATUS_CODE_TOPIC)
+            .selectKey((k, v) -> ((SchStatusCode) v).getSchoolCode());
 
-    KStream<SchoolId, Object> mergedStream = orgTopicStream.merge(subTypeStream).merge(statusCodeStream);
+    KStream<SchoolId, Object> mergedStream = orgTopicStream
+            .merge(subTypeStream)
+            .merge(statusCodeStream);
 
-    val aggregateStream = mergedStream.groupByKey().aggregate(SchoolAggregate::new, (key, value, schoolAggregate) -> {
+    KTable<SchoolId, SchoolAggregate> aggregate = mergedStream
+            .groupByKey()
+            .aggregate(SchoolAggregate::new, (key, value, schoolAggregate) -> {
       if (value instanceof OrgUnit) {
         schoolAggregate.setOrg((OrgUnit) value);
       } else if (value instanceof SchSubtype) {
         schoolAggregate.setType((SchSubtype) value);
       } else if (value instanceof SchStatusCode) {
-        schoolAggregate.setStatus((SchStatusCode) value);
+        schoolAggregate.setStatusCode((SchStatusCode) value);
       }
       return schoolAggregate;
-    });
-//
-//    ValueTransformerWithKeySupplier<SchoolId, Object, Object> valueTransformerWithKeySupplier = new ValueTransformerWithKeySupplier<>() {
-//      @Override
-//      public ValueTransformerWithKey<SchoolId, Object, Object> get() {
-//        return null;
-//      }
-//    };
+    }, Materialized.as(SCHOOL_AGGREGATE_STORE));
 
-    aggregateStream.toStream().transformValues(() -> {
+
+    windowSuppressTechnique(aggregate);
+
+    windowSuppressTechniqueRessetting(aggregate, storeProvider);
+
+    customProcessorSuppressTechnique(aggregate);
+  }
+
+  /**
+   * Simple but the timer doesn't reset when a new aggregate is received
+   * @param aggregateStream
+   * @return
+   */
+  private KStream<SchoolId, SchoolAggregate> windowSuppressTechnique(KTable<SchoolId, SchoolAggregate> aggregateStream) {
+    val suppress = Suppressed.untilTimeLimit(suppressionWindowTime, unbounded());
+    KTable<SchoolId, SchoolAggregate> stream = aggregateStream.suppress(suppress);
+    return stream.toStream();
+  }
+
+  /**
+   * Complex, but time resets upon new aggregates, however runs on stream time and so needs arrival of new records to trigger.
+   *
+   * @param aggregateStream
+   * @param storeProvider
+   * @return
+   */
+  private KStream<SchoolId, SchoolAggregate> windowSuppressTechniqueRessetting(KTable<SchoolId, SchoolAggregate> aggregateStream,
+                                                                               StoreProvider storeProvider) {
+    val suppressionWindow = TimeWindows.of(suppressionWindowTime);
+    Suppressed<Windowed> suppression = Suppressed.untilWindowCloses(unbounded());
+
+    val windowStream = aggregateStream
+            .toStream()
+            .groupByKey()
+            .windowedBy(suppressionWindow)
+            .reduce((value1, value2) -> value2)
+            .suppress(suppression);
+
+    return windowStream
+            .toStream()
+            .map((windowedKey, suppressedWindowedValue) -> {
+      SchoolId key = windowedKey.key();
+      ReadOnlyKeyValueStore<SchoolId, SchoolAggregate> aggregateStore = storeProvider.getStore(SCHOOL_AGGREGATE_STORE, QueryableStoreTypes.keyValueStore());
+      SchoolAggregate schoolAggregateValueAndTimestamp = aggregateStore.get(key);
+      val pair = KeyValue.pair(key, schoolAggregateValueAndTimestamp);
+      return pair;
+    });
+  }
+
+  /**
+   * Complex but runs on wall clock time.
+   *
+   * @param aggregateStream
+   * @return
+   */
+  private KStream<SchoolId, SchoolAggregate> customProcessorSuppressTechnique(KTable<SchoolId, SchoolAggregate> aggregateStream) {
+    return aggregateStream.toStream().transformValues(() -> {
       return new ValueTransformerWithKey<SchoolId, SchoolAggregate, SchoolAggregate>() {
 
         private Duration SUPPRESSION_TIME = Duration.ofSeconds(10);
         private KeyValueStore<SchoolId, SchoolAggregate> aggregateStore;
         private KeyValueStore<Long, SchoolId> suppressionStore;
-        private int expectedRecordsCount = 3;
+        private Duration SUPPRESSION_CHECK_FREQUENCY = Duration.ofSeconds(15);
 
         @Override
         public void init(ProcessorContext context) {
           this.suppressionStore = (KeyValueStore<Long, SchoolId>) context.getStateStore("supressionStore");
           this.aggregateStore = (KeyValueStore<SchoolId, SchoolAggregate>) context.getStateStore("aggregateStore");
 
-          context.schedule(Duration.ofSeconds(10), PunctuationType.WALL_CLOCK_TIME, timestamp -> {
+
+          context.schedule(SUPPRESSION_CHECK_FREQUENCY, PunctuationType.WALL_CLOCK_TIME, timestamp -> {
             long expireTime = System.currentTimeMillis() - SUPPRESSION_TIME.toMillis();
             KeyValueIterator<Long, SchoolId> expiredEntries = suppressionStore.range(0l, expireTime); // ready for emission
             expiredEntries.forEachRemaining(entry -> {
               SchoolId key = entry.value;
               SchoolAggregate schoolAggregate = aggregateStore.get(key);
               context.forward(key, schoolAggregate);
+              suppressionStore.delete(expireTime);
+              aggregateStore.delete(key);
             });
           });
         }
 
         @Override
         public SchoolAggregate transform(SchoolId key, SchoolAggregate value) {
-          aggregateStore.put(key, value);
+          aggregateStore.put(key, value); // store the aggregation
+          // update the last seen time stamp for suppression
+          long now = System.currentTimeMillis();
+          suppressionStore.put(now, key);
           return null; // don't emit here, see punctuator
         }
 
@@ -86,14 +150,7 @@ public class SchoolTopology {
           suppressionStore.close();
         }
       };
-    }, "aggregate-dedupper");
-  }
-
-  @Data
-  class SchoolAggregate {
-    private OrgUnit org;
-    private SchSubtype type;
-    private SchStatusCode status;
+    }, "aggregate-dedupper").through(AGGREGATE_UPDATES_TOPIC);
   }
 
 }
--- streams-module/src/test/java/io/confluent/ps/streams/referenceapp/school/SchoolTopologyTest.java
+++ streams-module/src/test/java/io/confluent/ps/streams/referenceapp/school/SchoolTopologyTest.java
@@ -1,12 +1,10 @@
 package io.confluent.ps.streams.referenceapp.school;
 
 import com.github.jukkakarvanen.kafka.streams.test.TestInputTopic;
+import com.github.jukkakarvanen.kafka.streams.test.TestOutputTopic;
 import com.github.jukkakarvanen.kafka.streams.test.TopologyTestDriver;
 import io.confluent.ps.streams.referenceapp.finance.TestDataDriver;
-import io.confluent.ps.streams.referenceapp.schools.model.OrgUnit;
-import io.confluent.ps.streams.referenceapp.schools.model.SchStatusCode;
-import io.confluent.ps.streams.referenceapp.schools.model.SchSubtype;
-import io.confluent.ps.streams.referenceapp.schools.model.SchoolId;
+import io.confluent.ps.streams.referenceapp.schools.model.*;
 import io.confluent.ps.streams.referenceapp.schools.topology.SchoolTopology;
 import io.confluent.ps.streams.referenceapp.tests.GuiceInjectedTestBase;
 import io.confluent.ps.streams.referenceapp.utils.KSUtils;
@@ -15,6 +13,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SchoolTopologyTest extends GuiceInjectedTestBase {
 
@@ -30,6 +32,7 @@ public class SchoolTopologyTest extends GuiceInjectedTestBase {
   TestInputTopic<SchoolId, OrgUnit> orgTopic;
   TestInputTopic<SchoolId, SchStatusCode> statusTopic;
   TestInputTopic<SchoolId, SchSubtype> subtypeTopic;
+  private TestOutputTopic<SchoolId, SchoolAggregate> aggTopic;
 
   @BeforeEach
   void setup() {
@@ -39,19 +42,30 @@ public class SchoolTopologyTest extends GuiceInjectedTestBase {
     var orgSerde = ksutils.<OrgUnit>serdeFor(false);
     var statusSerde = ksutils.<SchStatusCode>serdeFor(false);
     var subtypeSerde = ksutils.<SchSubtype>serdeFor(false);
+    var aggSerde = ksutils.<SchoolAggregate>serdeFor(false);
 
-    orgTopic = td.createInputTopic(SchoolTopology.ORG_TOPIC, keySerde.serializer(), orgSerde.serializer());
+    orgTopic = td.createInputTopic(SchoolTopology.ORG_UNIT_TOPIC, keySerde.serializer(), orgSerde.serializer());
     statusTopic = td.createInputTopic(SchoolTopology.STATUS_CODE_TOPIC, keySerde.serializer(), statusSerde.serializer());
     subtypeTopic = td.createInputTopic(SchoolTopology.SUP_TYPE_TOPIC, keySerde.serializer(), subtypeSerde.serializer());
 
-    td.createOutputTopic(SchoolTopology.AGGREGATE_UPDATES_TOPIC, keySerde.deserializer(), keySerde.deserializer());
+    aggTopic = td.createOutputTopic(SchoolTopology.AGGREGATE_UPDATES_TOPIC, keySerde.deserializer(), aggSerde.deserializer());
   }
 
   @Test
   void dataAggregator() {
+    // send through some data
     val schoolId = SchoolId.newBuilder().setId("a-school").build();
     val build = OrgUnit.newBuilder().setCode("code").setSchoolCode(schoolId).setName("").build();
     orgTopic.pipeInput(schoolId, build);
+
+    // check records are suppressed
+    assertThat(aggTopic.readValuesToList()).hasSize(0);
+
+    // move time forward
+
+    // check record emitted
+    List<SchoolAggregate> records = aggTopic.readValuesToList();
+    assertThat(records).hasSize(1);
   }
 
 }
